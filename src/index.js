8f1d4593f032c36f38d6af2972f8fdb571caa839
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-11-19 00:16:46 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ import { createMarkup } from './js/markup';
 import { renderMarkup } from './js/markup';
 
 let page = 1;
-const perPage = 20;
+const perPage = 40;
 //let query = '';
-
-//refs.buttonLoadMoreEl.classList.add('is-hidden');
 
 function showLoader(loader) {
   refs.buttonLoadMoreEl.classList.remove('is-hidden');
@@ -28,47 +26,6 @@ const instance = new SimpleLightbox('.gallery a', {
   captionDelay: '250',
 });
 
-// async function handlerSearchImages(event) {
-//   event.preventDefault();
-//   refs.galleryEl.innerHTML = '';
-//    clearMarkup(refs.galleryEl, refs.buttonLoadMoreEl);
-//   query = event.currentTarget.elements.searchQuery.value.trim().toLowerCase();
-//   if (!query || query === ' ') {
-//     hideLoader();
-//     return Notiflix.Notify.info(`Sorry, write your request`);
-//     hideLoader();
-//     refs.buttonLoadMoreEl.classList.remove('is-hidden');
-//   }
-//   page = 1;
-//   showLoader();
-//   const response = await fetchImages(query, page, perPage)
-//     .then(images => {
-//       refs.galleryEl.insertAdjacentHTML('beforeend', createMarkup(images.hits));
-//       const images = response.data.hits;
-//       totalHits = response.data.totalHits;
-//       console.log(response);
-
-//       images.totalHits > 0 && images.hits.length > 0
-//         ? Notiflix.Notify.success(
-//             `Hooray! We found ${images.totalHits} images.`
-//           )
-//         : Notiflix.Notify.failure(
-//             `Sorry, there are no images matching your search query. Please try again`
-//           );
-
-//       if (images.totalHits > perPage && images.hits.length > 0) {
-//         refs.buttonLoadMoreEl.classList.remove('is-hidden');
-//       }
-//       instance.refresh();
-//     })
-//     .catch(error => {
-//       Notiflix.Notify.failure(
-//         `Oops, something went wrong. Please try again later.`
-//       );
-//     })
-//     .finally(refs.formEl.reset(), hideLoader());
-// }
-
 async function handlerSearchImages(event) {
   event.preventDefault();
   refs.galleryEl.innerHTML = '';
@@ -84,6 +41,7 @@ async function handlerSearchImages(event) {
   }
   let page = 1;
   showLoader();
+
   const response = await fetchImages(query, page, perPage)
     .then(images => {
       console.log(images);
@@ -100,6 +58,10 @@ async function handlerSearchImages(event) {
       if (images.totalHits > perPage && images.hits.length > 0) {
         showLoader();
       }
+      if (images.totalHits < perPage) {
+        hideLoader();
+        refs.formEl.reset();
+      }
       instance.refresh();
     })
     .catch(error => {
@@ -107,80 +69,35 @@ async function handlerSearchImages(event) {
         `Oops, something went wrong. Please try again later.`
       );
     })
-    .finally(refs.formEl.reset(), hideLoader());
+    .finally(hideLoader());
 }
+
 function clearMarkup(gallery) {
   refs.galleryEl.innerHTML = ' ';
-  refs.buttonLoadMoreEl.classList.add('is-hidden');
 }
 
 async function loadMore() {
   page += 1;
-  // let query = refs.formEl.elements.searchQuery.value.trim().toLowerCase();
+  let query = refs.formEl.elements.searchQuery.value.trim().toLowerCase();
   const response = await fetchImages(query, page, perPage);
   let showPage = response.totalHits / perPage;
 
   if (showPage <= page) {
     hideLoader();
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
+    refs.formEl.reset(),
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
   }
   await fetchImages(query, page, perPage).then(images => {
-    console.log(images);
-    //renderMarkup(images.hits);
-    //instance.refresh();
+    console.log(images.hits);
+    renderMarkup(images.hits);
+    instance.refresh();
   });
 }
 
-// async function loadMore() {
-//   page += 1;
-//   let query = refs.formEl.elements.searchQuery.value.trim().toLowerCase();
-
-//   const response = await fetchImages(query, page, perPage)
-//     //let showPage = response.data.totalHits / perPage;
-//     .then(images => {
-//       console.log(images);
-//     });
-//   if (showPage <= page) {
-//     hideLoader();
-//     Notiflix.Notify.info(
-//       "We're sorry, but you've reached the end of search results."
-//     );
-//   }
-//   renderMarkup(response.data.hits);
-// } catch (error) {
-//   Notiflix.Notify.failure(
-//     'Sorry, there are no images matching your search query. Please try again.'
-//   );
-// }
-// instance.refresh();
-//}
-
-// async function loadMore() {
-//   page += 1;
-//   let query = refs.formEl.elements.searchQuery.value.trim().toLowerCase();
-
-//   try {
-//     const response = await fetchImages(query, page, perPage);
-//     let showPage = response.data.totalHits / perPage;
-
-//     if (showPage <= page) {
-//       hideLoader();
-//       Notiflix.Notify.info(
-//         "We're sorry, but you've reached the end of search results."
-//       );
-//     }
-//     renderMarkup(response.data.hits);
-//   } catch (error) {
-//     Notiflix.Notify.failure(
-//       'Sorry, there are no images matching your search query. Please try again.'
-//     );
-//   }
-//   instance.refresh();
-// }
 function renderMarkup(images) {
   refs.galleryEl.insertAdjacentHTML('beforeend', createMarkup(images));
 }
 
-console.log('HELLO world');
+console.log('Соня коза');
